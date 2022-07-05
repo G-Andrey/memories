04e823883b0648001d14bfd44d2c6075e32f6da9
unknown <g.andrey77@gmail.com>
2022-07-05 17:40:06 -0400

    updated api url to hosted backend

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API = axios.create({ baseURL: 'http://localhost:5000'});
+const API = axios.create({ baseURL: 'https://social-media-memories-app.herokuapp.com/'});
 
 //function that runs before every API call
 API.interceptors.request.use((req) =>{
